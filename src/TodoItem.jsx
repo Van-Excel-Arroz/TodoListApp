cbd397d808ea3ce399e66da16bc1ba89dc918309
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-04-30 16:49:03 +0800

    Include a key prop on ListItem component

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,50 +1,44 @@
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import {
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Checkbox,
-  IconButton,
-} from '@mui/material';
+import { ListItem, ListItemButton, ListItemIcon, ListItemText, Checkbox, IconButton } from '@mui/material';
 
 export default function TodoItem({ todo, onHandleToggle, onRemoveTodo }) {
-  const labelId = `checkbox-list-label-${todo.id}`;
+	const labelId = `checkbox-list-label-${todo.id}`;
 
-  return (
-    <ListItem
-      secondaryAction={
-        <IconButton edge="end" aria-label="delete" onClick={() => onRemoveTodo(todo.id)}>
-          <DeleteOutlineIcon sx={{ color: '#f54d4d' }} />
-        </IconButton>
-      }
-      disablePadding
-    >
-      <ListItemButton role={undefined} dense onClick={() => onHandleToggle(todo.id)}>
-        <ListItemIcon>
-          <Checkbox
-            edge="start"
-            checked={todo.completed}
-            // onChange={() => onHandleToggle(todo.id)}
-            tabIndex={-1}
-            disableRipple
-            inputProps={{ 'aria-labelledby': labelId }}
-          />
-        </ListItemIcon>
-        <ListItemText
-          style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-          id={labelId}
-          primary={todo.text}
-          primaryTypographyProps={{
-            style: {
-              fontSize: '1.3rem',
-              maxWidth: 'fit-content',
-              wordBreak: 'break-all',
-              paddingRight: 8,
-            },
-          }}
-        />
-      </ListItemButton>
-    </ListItem>
-  );
+	return (
+		<ListItem
+			key={todo.id}
+			secondaryAction={
+				<IconButton edge="end" aria-label="delete" onClick={() => onRemoveTodo(todo.id)}>
+					<DeleteOutlineIcon sx={{ color: '#f54d4d' }} />
+				</IconButton>
+			}
+			disablePadding
+		>
+			<ListItemButton role={undefined} dense onClick={() => onHandleToggle(todo.id)}>
+				<ListItemIcon>
+					<Checkbox
+						edge="start"
+						checked={todo.completed}
+						// onChange={() => onHandleToggle(todo.id)}
+						tabIndex={-1}
+						disableRipple
+						inputProps={{ 'aria-labelledby': labelId }}
+					/>
+				</ListItemIcon>
+				<ListItemText
+					style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+					id={labelId}
+					primary={todo.text}
+					primaryTypographyProps={{
+						style: {
+							fontSize: '1.3rem',
+							maxWidth: 'fit-content',
+							wordBreak: 'break-all',
+							paddingRight: 8,
+						},
+					}}
+				/>
+			</ListItemButton>
+		</ListItem>
+	);
 }
